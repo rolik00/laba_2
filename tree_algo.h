989e686b2bcbd29f9985154c4dea1dcc0dcded67
rolik00 <148611487+rolik00@users.noreply.github.com>
2024-04-28 14:42:08 +0300

    Update tree_algo.h

--- tree_algo.h
+++ tree_algo.h
@@ -1,4 +1,3 @@
-#pragma once
 #include "help_function.h"
 
 struct Event
@@ -6,27 +5,16 @@ struct Event
     int x;
     int startY;
     int endY;
-    int status; // 1 - начало прямоугольника, -1 - конец прямоугольника
+    int status; // 1 - РЅР°С‡Р°Р»Рѕ РїСЂСЏРјРѕСѓРіРѕР»СЊРЅРёРєР°, -1 - РєРѕРЅРµС† РїСЂСЏРјРѕСѓРіРѕР»СЊРЅРёРєР°
     Event(int x, int y1, int y2, int st) : x(x), startY(y1), endY(y2), status(st) {}
 };
 
 struct Node
 {
-    int count; // количество прямоугольников на отрезке
-    int left_index;
-    int right_index;
-    Node* left;
-    Node* right;
-    Node() : count(0), left_index(0), right_index(0), left(nullptr), right(nullptr) {}
-    Node(int count, int l_index, int r_index, Node* l, Node* r) : count(count), left_index(l_index), right_index(r_index), left(l), right(r) {}
+    int count; // РєРѕР»РёС‡РµСЃС‚РІРѕ РїСЂСЏРјРѕСѓРіРѕР»СЊРЅРёРєРѕРІ РЅР° РѕС‚СЂРµР·РєРµ
+    std::shared_ptr<Node> left, right;
+    Node(int count, std::shared_ptr<Node> left, std::shared_ptr<Node> right) : count(count), left(left), right(right) {}
+    Node() : count(0), left(nullptr), right(nullptr) {}
 };
-
-Node* build_segment_tree(int left, int right);
-
-Node* insert(Node* node, int start, int end, int status);
-
-std::vector<Node*> build_persistent_segment_tree(std::vector<std::vector<int>> rectangles, std::vector<int> coordsY);
-
-int get_number(Node* node, int target);
 
 void tree_algo();
